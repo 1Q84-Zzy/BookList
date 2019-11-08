1e4669a988a001525f91e1067fbb42b93358a3c8
1Q84-Zzy <Zziyi520@foxmail.com>
2019-11-08 10:45:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/casper/TextViewMainActivity.java
@@ -0,0 +1,32 @@
+package com.example.casper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class TextViewMainActivity extends AppCompatActivity {
+
+    private TextView textViewHelloWorld;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_view_main);
+
+        textViewHelloWorld=findViewById(R.id.textViewHelloWorld);
+        textViewHelloWorld.setText("你好，编程实现的Hello world!");
+
+        textViewHelloWorld.setText(R.string.hello_world2);     //直接设置字符串资源id
+
+
+        textViewHelloWorld.setText(this.getString(R.string.hello_world));   //通过资源id获得字符串
+
+        //通过资源名称获得字符串，如何通过"hello_world"获得R.string.hello_world
+        Context context=this.getApplicationContext();
+        String resName="hello_world";
+        int helloWorldId=context.getResources().getIdentifier(resName, "string", context.getPackageName());
+        textViewHelloWorld.setText(this.getString(helloWorldId));
+
+    }
+}
